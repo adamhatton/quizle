58b7d309b8bc1dbb90d2c497dde85e6047a8ea6c
adamhatton <adamdhatton@gmail.com>
2022-11-12 14:08:07 +0000

    Add basic routing to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import NavBar from './components/NavBar';
 import styles from './App.module.css';
+import {Route, Switch} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar></NavBar>
-      <h1>Quizle</h1>
+      <Container className={styles.Main} >
+        <Switch>
+          <Route exact path='/' render={() => <h1>Quizzes</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path='/create' render={() => <h1>Quiz Creation</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path='/profile' render={() => <h1>Profile</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path='/signin' render={() => <h1>Sign In</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path='/signup' render={() => <h1>Sign Up</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
