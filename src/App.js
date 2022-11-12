10aa62e90d58425a399d5db195817e402ab25b30
adamhatton <adamdhatton@gmail.com>
2022-11-12 15:24:35 +0000

    Add 404 route

--- src/App.js
+++ src/App.js
@@ -10,21 +10,12 @@ function App() {
       <Container className={styles.Main} >
         <Switch>
           <Route exact path='/' render={() => <h1>Quizzes</h1>} />
-        </Switch>
-        <Switch>
           <Route exact path='/create' render={() => <h1>Quiz Creation</h1>} />
-        </Switch>
-        <Switch>
           <Route exact path='/profile' render={() => <h1>Profile</h1>} />
-        </Switch>
-        <Switch>
           <Route exact path='/signin' render={() => <h1>Sign In</h1>} />
-        </Switch>
-        <Switch>
           <Route exact path='/signup' render={() => <h1>Sign Up</h1>} />
-        </Switch>
-        <Switch>
           <Route exact path='/signout' render={() => <h1>Sign Out</h1>} />
+          <Route render={() => <h1>Page not found!</h1>} />
         </Switch>
       </Container>
     </div>
