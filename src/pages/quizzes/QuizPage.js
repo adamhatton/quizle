429ffd6aa458c6d968b880e39c9ac9fadd344d92
adamhatton <adamdhatton@gmail.com>
2022-11-13 19:47:26 +0000

    Add jsx structure for QuizPage

--- src/pages/quizzes/QuizPage.js
+++ src/pages/quizzes/QuizPage.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import { useParams } from 'react-router-dom'
 import { axiosReq } from '../../api/axiosDefaults';
+import Avatar from '../../components/Avatar';
 
 function QuizPage() {
   const { id } = useParams();
@@ -21,7 +24,42 @@ function QuizPage() {
   }, [id]);
 
   return (
-    <div>QuizPage</div>
+    <div>
+        <h1>Quiz Title</h1>
+        <p>Quiz Description</p>
+        <Row>
+          <Col xs={1}>
+            <Avatar />
+          </Col>
+          <Col xs={2}>
+            <h2>Created By</h2>
+            <p>Creators' Name</p>
+          </Col>
+          <Col xs={1}>
+            <p>X</p>
+          </Col>
+          <Col xs={4}>
+            <h2>High Score</h2>
+            <p>You haven't completed this quiz yet!</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p>Search Bar</p>
+          </Col>
+          <Col>
+            <p>Timer</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6} lg={4}>
+            <h2>Hints</h2>
+          </Col>
+          <Col xs={6} lg={4}>
+            <h2>Answers</h2>
+          </Col>
+        </Row>    
+    </div>
   )
 }
 
