bb895e5602ef76b43f0d22e1548d6389af5f8214
adamhatton <adamdhatton@gmail.com>
2022-11-13 14:33:37 +0000

    Add hint and answer inputs to quiz form

--- src/pages/quizzes/QuizCreateForm.js
+++ src/pages/quizzes/QuizCreateForm.js
@@ -104,13 +104,316 @@ function QuizCreateForm() {
           <Alert variant="warning" key={idx}>{message}</Alert>
         )} */}
 
+        <Row>
+            <Col sm={4}>
+                <h2>Hints</h2>
+            </Col>
+            <Col sm={4}>
+                <h2>Answers</h2>
+            </Col>
+        </Row>
+
         <Form.Row>
-          <Form.Group as={Col} controlId='hint_1'>
+          <Form.Group as={Col} sm={4} controlId='hint_1'>
             <Form.Label srOnly>Hint 1</Form.Label>
             <Form.Control
               type='text'
               placeholder='Hint 1'
               name='hint_1'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */}
+
+          <Form.Group as={Col} sm={4} controlId='ans_1'>
+            <Form.Label srOnly>Answer 1</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Answer 1'
+              name='ans_1'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */} 
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} sm={4} controlId='hint_2'>
+            <Form.Label srOnly>Hint 2</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Hint 2'
+              name='hint_2'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */}
+
+          <Form.Group as={Col} sm={4} controlId='ans_2'>
+            <Form.Label srOnly>Answer 2</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Answer 2'
+              name='ans_2'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */} 
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} sm={4} controlId='hint_3'>
+            <Form.Label srOnly>Hint 3</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Hint 3'
+              name='hint_3'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */}
+
+          <Form.Group as={Col} sm={4} controlId='ans_3'>
+            <Form.Label srOnly>Answer 3</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Answer 3'
+              name='ans_3'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */} 
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} sm={4} controlId='hint_4'>
+            <Form.Label srOnly>Hint 4</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Hint 4'
+              name='hint_4'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */}
+
+          <Form.Group as={Col} sm={4} controlId='ans_4'>
+            <Form.Label srOnly>Answer 4</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Answer 4'
+              name='ans_4'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */} 
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} sm={4} controlId='hint_5'>
+            <Form.Label srOnly>Hint 5</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Hint 5'
+              name='hint_5'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */}
+
+          <Form.Group as={Col} sm={4} controlId='ans_5'>
+            <Form.Label srOnly>Answer 5</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Answer 5'
+              name='ans_5'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */} 
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} sm={4} controlId='hint_6'>
+            <Form.Label srOnly>Hint 6</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Hint 6'
+              name='hint_6'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */}
+
+          <Form.Group as={Col} sm={4} controlId='ans_6'>
+            <Form.Label srOnly>Answer 6</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Answer 6'
+              name='ans_6'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */} 
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} sm={4} controlId='hint_7'>
+            <Form.Label srOnly>Hint 7</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Hint 7'
+              name='hint_7'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */}
+
+          <Form.Group as={Col} sm={4} controlId='ans_7'>
+            <Form.Label srOnly>Answer 7</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Answer 7'
+              name='ans_7'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */} 
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} sm={4} controlId='hint_8'>
+            <Form.Label srOnly>Hint 8</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Hint 8'
+              name='hint_8'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */}
+
+          <Form.Group as={Col} sm={4} controlId='ans_8'>
+            <Form.Label srOnly>Answer 8</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Answer 8'
+              name='ans_8'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */} 
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} sm={4} controlId='hint_9'>
+            <Form.Label srOnly>Hint 9</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Hint 9'
+              name='hint_9'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */}
+
+          <Form.Group as={Col} sm={4} controlId='ans_9'>
+            <Form.Label srOnly>Answer 9</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Answer 9'
+              name='ans_9'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */} 
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} sm={4} controlId='hint_10'>
+            <Form.Label srOnly>Hint 10</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Hint 10'
+              name='hint_10'
+              //   value={username}
+              //   className={styles.Input}
+              //   onChange={handleChange}
+            />
+          </Form.Group>
+          {/* {errors.username?.map((message, idx) =>
+          <Alert variant="warning" key={idx}>{message}</Alert>
+          )} */}
+
+          <Form.Group as={Col} sm={4} controlId='ans_10'>
+            <Form.Label srOnly>Answer 10</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Answer 10'
+              name='ans_10'
               //   value={username}
               //   className={styles.Input}
               //   onChange={handleChange}
