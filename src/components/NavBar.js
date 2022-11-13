431794db552a9badaada8df172afbe047e4fe9db
adamhatton <adamdhatton@gmail.com>
2022-11-13 12:07:42 +0000

    Refactor toggle function into a hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
@@ -7,29 +7,14 @@ import styles from '../styles/NavBar.module.css';
 import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
 import Avatar from './Avatar';
 import axios from 'axios';
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
-  const ref = useRef(null);
-
-  const [expanded, setExpanded] = useState(false);
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (ref.current && !ref.current.contains(event.target)){
-        setExpanded(false);
-      }
-    }
-
-    document.addEventListener('mouseup', handleClickOutside);
-
-    return () => {
-      document.removeEventListener('mouseup', handleClickOutside)
-    }
-
-  }, [ref]);
+  
+  const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try{
---
+++ src/hooks/useClickOutsideToggle.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const useClickOutsideToggle = () => {
+    const ref = useRef(null);
+
+    const [expanded, setExpanded] = useState(false);
+  
+    useEffect(() => {
+      const handleClickOutside = (event) => {
+        if (ref.current && !ref.current.contains(event.target)){
+          setExpanded(false);
+        }
+      }
+  
+      document.addEventListener('mouseup', handleClickOutside);
+  
+      return () => {
+        document.removeEventListener('mouseup', handleClickOutside)
+      }
+  
+    }, [ref]);
+
+  return { expanded, setExpanded, ref }
+}
+
+export default useClickOutsideToggle
