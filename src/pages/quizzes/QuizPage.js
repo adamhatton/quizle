a15c8635ee407dba611265a0e8c1aa768317331c
adamhatton <adamdhatton@gmail.com>
2022-11-22 21:48:37 +0000

    Add comment API call and output ternary

--- src/pages/quizzes/QuizPage.js
+++ src/pages/quizzes/QuizPage.js
@@ -6,6 +6,7 @@ import Asset from '../../components/Asset';
 import Quiz from './Quiz';
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 /* Component to obtain quiz information and display it*/
 function QuizPage() {
@@ -24,36 +25,40 @@ function QuizPage() {
   useEffect(() => {
     const handleMount = async () => {
         try {
-            const {data} = await axiosReq.get(`/quizzes/${id}`)
+            const [ {data: quiz}, {data: comments} ] = await Promise.all([
+              axiosReq.get(`/quizzes/${id}`),
+              axiosReq.get(`/comments/?quiz=${id}`)
+            ])
+            setComments(comments);
             setQuizInfo({
-              id: data.id,
-              owner: data.owner,
-              title: data.title,
-              description: data.description,
-              category: data.category,
-              time_limit_seconds: data.time_limit_seconds,
-              created_on: data.created_on,
-              updated_on: data.updated_on,
-              profile_id: data.profile_id,
-              profile_image: data.profile_image,
-              like_id: data.like_id,
-              score_id: data.score_id,
-              score_time: data.score_time,
-              likes_count: data.likes_count,
-              comments_count: data.comments_count,
-              completed_count: data.completed_count,
+              id: quiz.id,
+              owner: quiz.owner,
+              title: quiz.title,
+              description: quiz.description,
+              category: quiz.category,
+              time_limit_seconds: quiz.time_limit_seconds,
+              created_on: quiz.created_on,
+              updated_on: quiz.updated_on,
+              profile_id: quiz.profile_id,
+              profile_image: quiz.profile_image,
+              like_id: quiz.like_id,
+              score_id: quiz.score_id,
+              score_time: quiz.score_time,
+              likes_count: quiz.likes_count,
+              comments_count: quiz.comments_count,
+              completed_count: quiz.completed_count,
             });
             setQuizAnswers([
-              { id: 1, hint: data.hint_1, value: data.ans_1, guessed: false, revealed: false },
-              { id: 2, hint: data.hint_2, value: data.ans_2, guessed: false, revealed: false },
-              { id: 3, hint: data.hint_3, value: data.ans_3, guessed: false, revealed: false },
-              { id: 4, hint: data.hint_4, value: data.ans_4, guessed: false, revealed: false },
-              { id: 5, hint: data.hint_5, value: data.ans_5, guessed: false, revealed: false },
-              { id: 6, hint: data.hint_6, value: data.ans_6, guessed: false, revealed: false },
-              { id: 7, hint: data.hint_7, value: data.ans_7, guessed: false, revealed: false },
-              { id: 8, hint: data.hint_8, value: data.ans_8, guessed: false, revealed: false },
-              { id: 9, hint: data.hint_9, value: data.ans_9, guessed: false, revealed: false },
-              { id: 10, hint: data.hint_10, value: data.ans_10, guessed: false, revealed: false },
+              { id: 1, hint: quiz.hint_1, value: quiz.ans_1, guessed: false, revealed: false },
+              { id: 2, hint: quiz.hint_2, value: quiz.ans_2, guessed: false, revealed: false },
+              { id: 3, hint: quiz.hint_3, value: quiz.ans_3, guessed: false, revealed: false },
+              { id: 4, hint: quiz.hint_4, value: quiz.ans_4, guessed: false, revealed: false },
+              { id: 5, hint: quiz.hint_5, value: quiz.ans_5, guessed: false, revealed: false },
+              { id: 6, hint: quiz.hint_6, value: quiz.ans_6, guessed: false, revealed: false },
+              { id: 7, hint: quiz.hint_7, value: quiz.ans_7, guessed: false, revealed: false },
+              { id: 8, hint: quiz.hint_8, value: quiz.ans_8, guessed: false, revealed: false },
+              { id: 9, hint: quiz.hint_9, value: quiz.ans_9, guessed: false, revealed: false },
+              { id: 10, hint: quiz.hint_10, value: quiz.ans_10, guessed: false, revealed: false },
             ]);
             setHasLoaded(true);
         } catch(err){
@@ -86,6 +91,27 @@ function QuizPage() {
       ) : comments.results.length ? (
         "Comments"
       ) : null}
+      {comments.results.length ? (
+        // <InfiniteScroll
+        //   children={comments.results.map((comment) => (
+        //     <Comment
+        //       key={comment.id}
+        //       {...comment} 
+        //       setPost={setPost}
+        //       setComments={setComments}
+        //     />
+        //   ))}
+        //   dataLength={comments.results.length}
+        //   loader={<Asset spinner />}
+        //   hasMore={!!comments.next}
+        //   next={() => fetchMoreData(comments, setComments)}
+        // />
+        comments.results.map(comment => <p>{comment.content}</p>)
+      ) : currentUser ? (
+        <span>No comments yet, be the first to comment!</span>
+      ) : (
+        <span>No comments yet, login to add a comment!</span>
+      )}
     </>
     ) : (
       <Container className='text-center'>
